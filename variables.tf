128416ebe9d25ed3c454aa9129b1d28c4d12d1d0
aminniktash2010 <80291034+aminniktash2010@users.noreply.github.com>
2024-07-11 17:11:18 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,15 +6,57 @@ variable "business_services" {
 }
 
 
+variable "escalation_policy" {
+  description = "Escalation policy details"
+  type = object({
+    name      = string
+    schedule  = string
+  })
+}
+
+variable "first_escalation_user" {
+  description = "Email of the user to escalate to after 2H"
+  type        = string
+}
+variable "second_escalation_user" {
+  description = "Email of the user to escalate to after 4H"
+  type        = string
+}
+
+variable "schedule" {
+  description = "Schedule details"
+  type = object({
+    name        = string
+    time_zone   = string
+    description = optional(string)
+    usa_users   = list(string)
+    india_users = list(string)
+  })
+}
+
+
+variable "event_orchestration" {
+  description = "Event orchestration details"
+  type = object({
+    name = string
+  })
+}
 
 variable "services" {
-  description = "List of services to be created"
-  type = map(object({
-    customer_name               = string
-    product_name                = string
-    service_id                  = string
-    match_summary               = string
-    match_source                = string
-    business_service_name       = string
+  description = "List of services to be created in PagerDuty"
+  type = list(object({
+    customer_name         = string
+    product_name          = string
+    service_id            = string
+    match_summary         = string
+    match_source          = string
+    business_service_name = string
+    additional_conditions = optional(list(object({
+      expression = string
+    })), [])
+    extension = optional(object({
+      name         = string
+      endpoint_url = string
+    }), null)
   }))
 }
