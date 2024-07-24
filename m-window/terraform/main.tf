80f5e9fc47a03da9f067ab96f1d81b5fa31ae5e4
aminniktash2010 <80291034+aminniktash2010@users.noreply.github.com>
2024-07-24 15:04:24 -0400

    Update main.tf

--- m-window/terraform/main.tf
+++ m-window/terraform/main.tf
@@ -8,15 +8,7 @@ terraform {
 }
 
 provider "pagerduty" {
-  token = "var.pd-pagerduty_token"
-}
-
-resource "pagerduty_maintenance_window" "example" {
-  count      = var.weeks
-  start_time = timeadd(var.start_date, "${count.index * 7 * 24 * 60 * 60}s")
-  end_time   = timeadd(timeadd(var.start_date, "${count.index * 7 * 24 * 60 * 60}s"), "2h")
-
-  services    = [data.pagerduty_service.t-services.id]
+  token = var.pd-pagerduty_token
 }
 
 
@@ -26,18 +18,37 @@ data "pagerduty_service" "t-services" {
   name = var.service_name
 }
 
+variable "pd-pagerduty_token" {
+  description = "please provide your tocken"
+  type  = string
+}
 variable "service_name" {
   description = "List of PagerDuty service IDs"
   type        = string
 }
+##########################################
 
 variable "weeks" {
   description = "Number of weeks for the maintenance window recurrence"
   type        = number
   default     = 5
 }
+
 variable "start_date" {
   description = "Fixed start date for the maintenance window (format: YYYY-MM-DDTHH:MM:SSZ)"
   type        = string
   default     = "2024-07-23T20:00:00Z"
+}
+
+variable "duration" {
+  description = "Duration of each maintenance window (format: 2h, 30m, etc.)"
+  type        = string
+  default     = "2h"
+}
+
+resource "pagerduty_maintenance_window" "example" {
+  count      = var.weeks
+  start_time = timeadd(var.start_date, "${count.index * 7 * 24 * 60 * 60}s")
+  end_time   = timeadd(timeadd(var.start_date, "${count.index * 7 * 24 * 60 * 60}s"), var.duration)
+  services   = [data.pagerduty_service.t-services.id]
 }
